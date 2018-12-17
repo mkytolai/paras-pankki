76a675d7e9457c2ee49333d8f6987fdb1ee400d1
Mika <mkytolai@gmail.com>
2018-12-17 19:53:59 +0200

    Stylistic changes

--- src/test/java/com/paras/pankki/account/AccountResourceTest.java
+++ src/test/java/com/paras/pankki/account/AccountResourceTest.java
@@ -4,26 +4,26 @@ import com.paras.pankki.Bank;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountResourceTest {
 
-    private static final Bank tbank = new Bank();
-
     @ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new AccountResource(tbank))
+            .addResource(new AccountResource(new Bank()))
             .build();
 
-    private Balance bal = new Balance(0, new Currency("EUR"));
     @Test
     public void test_account_balance() {
-        Balance actualbalance = resources
+        Balance expected = new Balance(0, new Currency("EUR"));
+
+        Balance actual = resources
                 .target("/account/Alma")
                 .request()
                 .get(Balance.class);
 
-                assertThat(actualbalance.getBalance()).isEqualTo(bal.getBalance());
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
